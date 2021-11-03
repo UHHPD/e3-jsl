53b0ed57fb4fa7dfcbc42540666b86b9efeae364
jomatthi <82223346+jomatthi@users.noreply.github.com>
2021-11-03 15:40:36 +0000

    +Exercise 2d

--- likelihood.cc
+++ likelihood.cc
@@ -32,13 +32,19 @@ int main() {
     ofstream fout_l("likelihood.txt");
     ofstream fout_log("nll.txt");
     ofstream fout_del("deltanll.txt");
+    ofstream fout_int("interval.txt");
     for(double mu = 0; mu < 6; mu+=0.01){
     fout_l << mu << " " << prob(daten, mu) << std :: endl;
     fout_log << mu << " " << -2*log(prob(daten,mu)) << std :: endl;
     fout_del << mu << " " << (-2*log(prob(daten,mu)))-(-2*log(prob(daten,3.11538))) << std :: endl;
+    if(-2*log((prob(daten,mu))/prob(daten,3.11538))<1.0){
+      fout_int << mu << " " << -2*log((prob(daten,mu))/prob(daten,3.11538)) << std :: endl;
+    }
     }
     
     fout_l.close();
+    fout_int.close();
+    fout_del.close();
     fin.close();
     
 }
